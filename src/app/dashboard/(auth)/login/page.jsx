a985acb04d139f8f107c422ab2d26bfe24ef2037
Oleg <onespovityi@gmail.com>
2023-08-04 16:57:25 +0300

    added register

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { signIn } from 'next-auth/react'
+import cls from './page.module.css'
 
 const Login = () => {
   return (
-    <div>
+    <div className={cls.container}>
       <button onClick={() => signIn("google")}>Login with Google</button>
     </div>
   )
--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import cls from './page.module.css'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 const Register = () => {
+  const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+      res.status === 201 &&
+        router.push("/dashboard/login?success=Account has been created");
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
+
+
   return (
-    <div>Register</div>
+    <div className={cls.container}>
+      <h1 className={cls.title}>Create an Account</h1>
+      <h2 className={cls.subtitle}>Please sign up to see dashboard</h2>
+      <form className={cls.form} onSubmit={handleSubmit}>
+        <input type='text' placeholder='Username' className={cls.input} required />
+        <input type='email' placeholder='E-mail' className={cls.input} required />
+        <input type='password' placeholder='Password' className={cls.input} required />
+        <button className={cls.button}>
+          Register
+        </button>
+        {error && "Something went wrong!"}
+      </form>
+      <span className={cls.or}> - OR - </span>
+      <Link className={cls.link} href='/dashboard/login'>Login with an existing Account</Link>
+    </div>
   )
 }
 
